fa155299031a8a1365143fbd307e2df79ce5e3f8
Lucas Zanella <me@lucaszanella.com>
2021-02-14 06:30:54 +0000

    devcontainer works

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,5 @@
-xhost + && sudo docker run --device=/dev/kvm -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v $PWD/../project:/home/project -v /mnt/android-dev-3:/home/project/disk -it aosp_builder /bin/bash && xhost -
+xhost + && sudo docker run --device=/dev/kvm -it -v /tmp/.X11-unix:/tmp/.X11-unix \
+-e DISPLAY=unix$DISPLAY -v $PWD/../project:/home/project \
+-v /mnt/android-dev-3:/home/project/disk \
+-v /home/$USER/Android:/home/project/sdk \
+-it aosp_builder /bin/bash && xhost -
